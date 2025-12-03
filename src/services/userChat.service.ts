4a3293a9ac29e178aa12b342d0d96d635f074532
Reign Mark Valdez <valdezreignmark@gmail.com>
2025-12-03 20:37:20 +0800

    Add user chat feature with repositories and migration
    Introduces the UserChat model and migration, along with controller, service, repository, and route files to support user chat functionality. Includes methods for creating, updating, and removing chat rooms, as well as message creation and room retrieval by user.

---
+++ src/services/userChat.service.ts
@@ -0,0 +1,8 @@
+import CacheUtil from "../utils/cache.util";
+
+export default class UserChatSvc {
+  static async findRoomChatByUserId(data: any) {
+    console.log(">>>>>>>>>>>>>>>>> ", data)
+  }
+
+}
