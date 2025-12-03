5244c6d80d6b24e611caa99bf200e9dced2d02be
Reign Mark Valdez <valdezreignmark@gmail.com>
2025-12-03 20:39:36 +0800

    Add UserChat model and socket event for user rooms
    Introduces the UserChat model to the Prisma schema for tracking user-room relationships. Updates the organization socket event handler to emit user room data on 'init_user' using the new UserChat service.

--- src/events/organization.events.ts
+++ src/events/organization.events.ts
@@ -1,19 +1,35 @@
 import { Server } from "socket.io";
+import UserChatSvc from "../services/userChat.service";
 
 export default (io: Server) => {
-    io.on("connection", (socket) => {
-        console.log("Client connected to /organization");
+  // io.on("connection", (socket) => {
+  //     console.log("Client connected to /organization");
 
-        socket.on("disconnect", () => {
-            console.log("Client disconnected from /organization");
-        });
+  //     socket.on("disconnect", () => {
+  //         console.log("Client disconnected from /organization");
+  //     });
 
-        socket.on("join", (data) => {
-            console.log("Client joined /organization", data);
-        });
+  //     socket.on("join", (data) => {
+  //         console.log("Client joined /organization", data);
+  //     });
 
-        socket.on("chat_message", (data) => {
-            console.log("Client sent chat message", data);
-        });
+  //     socket.on("chat_message", (data) => {
+  //         console.log("Client sent chat message", data);
+  //     });
+  // });
+
+  // io.on("connection", async (data: Record<any, string>) => {
+  //    await UserChatSvc.findRoomChatByUserId(data.userId);
+  // })
+  io.on("connection", (socket) => {
+    console.log("+++++++++++++ Client connected:", socket.id);
+// a6n_iburaLuiB4ETAAAB
+    socket.on("init_user", async (data: { userId: string }) => {
+      const room = await UserChatSvc.findRoomChatByUserId(data.userId);
+      console.log("+++++++++++++ roomroom:", room);
+
+      socket.emit("user_rooms", room);
     });
+
+  });
 };
