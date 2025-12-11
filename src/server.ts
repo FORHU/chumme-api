5f285aa12a73691324e832be103aa47171e4ac93
joelralph <joelralph06@gmail.com>
2025-12-11 17:30:28 +0800

    Revert "feat: add initial Socket.IO server with room management and message broadcasting."
    This reverts commit 274a95cbb55ef503e09626c40b9df5cbc3bfcb63.

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,8 @@
 // src/server.ts
-import http from "http";
-import { Server } from "socket.io";
 import app from "./app";
+
 import { PORT } from "./config";
-import initializeSocket from "./socket";
 
-const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-  },
-});
-
-// Initialize socket events
-initializeSocket(io);
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
