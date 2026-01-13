a2b117867b9f46c17218f623b4b34c862e5e109a
Reign Mark Valdez <valdezreignmark@gmail.com>
2026-01-13 14:36:16 +0800

    hot fix

--- src/config.ts
+++ src/config.ts
@@ -37,4 +37,4 @@ export const RABBITMQ_EXCHANGE =
   process.env.RABBITMQ_EXCHANGE || "chumme_exchange";
 export const RABBITMQ_QUEUE_PREFIX =
   process.env.RABBITMQ_QUEUE_PREFIX || "chumme";
-export const CHAT_WONDER_API_URL = process.env.CHAT_WONDER_API_URL;
+export const CHAT_WONDER_API_URL = process.env.CHAT_WONDER_API_URL as string;
