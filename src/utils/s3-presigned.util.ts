7d71a7625a96112b99249fd31aa73dfd720c1abc
mzton <markargel1903@gmail.com>
2026-09-10 11:55:24 +0800

    feat: refactor S3 client initialization and add IAM policy for S3 access

--- src/utils/s3-presigned.util.ts
+++ src/utils/s3-presigned.util.ts
@@ -1,24 +1,7 @@
-import {
-  S3Client,
-  PutObjectCommand,
-  GetObjectCommand,
-} from "@aws-sdk/client-s3";
+import { PutObjectCommand, GetObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
-import {
-  AWS_REGION,
-  AWS_ACCESS_KEY_ID,
-  AWS_SECRET_ACCESS_KEY,
-  AWS_S3_BUCKET_NAME,
-} from "../config";
-
-// Initialize the S3 Client using centralized configuration
-const s3Client = new S3Client({
-  region: AWS_REGION,
-  credentials: {
-    accessKeyId: AWS_ACCESS_KEY_ID,
-    secretAccessKey: AWS_SECRET_ACCESS_KEY,
-  },
-});
+import { AWS_S3_BUCKET_NAME } from "../config";
+import s3Client from "./s3-client";
 
 /**
  * Function to get an UPLOAD (PUT) URL
